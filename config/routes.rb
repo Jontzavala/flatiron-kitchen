a49e66525b3fd838f642182747ef54b95c6fa518
Jonathan Zavala <jontzavala@gmail.com>
2021-04-12 17:49:49 -0500

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  resources :recipe_ingredients, only: [:new, :create, :destroy]
+  resources :recipes
+  resources :ingredients
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
